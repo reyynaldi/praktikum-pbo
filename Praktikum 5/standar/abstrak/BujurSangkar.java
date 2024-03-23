3dfdfb67c1c870f58b57b0806e068e019fe535dd
reyynaldi <118532069+reyynaldi@users.noreply.github.com>
2024-03-23 13:46:08 +0700

    Delete Praktikum 5/standar directory

--- Praktikum 5/standar/abstrak/BujurSangkar.java
+++
@@ -1,8 +0,0 @@
-package standar.abstrak;
-
-public class BujurSangkar extends BangunDatar{
-    public double hitungLuas(double sisi){
-        return sisi*sisi;
-    }
-}
-
--- Praktikum 5/standar/interface/IArea.java
+++
@@ -1,4 +0,0 @@
-public interface IArea {
-    public double hitungLuas();    
-}
-
